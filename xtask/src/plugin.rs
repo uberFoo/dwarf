0ad0ea0ec8f764caa4c854d52022c47f7db240aa
Keith Star <uberfoo@me.com>
2023-11-30 14:10:15 -0700

    Build sarzak (#14)
    * first attempt
    
    * second attempt -- install sarzak
    
    * xtask is failing to create ~/.dwarf
    
    * update shared library copying code to be cross platform
    
    * debug likely lib naming problem
    
    * use the right branch...
    
    * cd dmarf?

--- xtask/src/plugin.rs
+++ xtask/src/plugin.rs
@@ -80,8 +80,21 @@ fn build_plugin(entry: &DirEntry, sh: &Shell, dwarf_home: &String) -> anyhow::Re
     fs::create_dir_all(&src_dir)?;
     fs::create_dir_all(&model_dir)?;
 
-    println!("Copying lib{}.dylib", name);
-    sh.copy_file(format!("target/debug/lib{}.dylib", name), lib_dir)?;
+    let mut fubar = current_dir.clone();
+    fubar.push("target");
+    fubar.push("debug");
+    dbg!(sh.read_dir(fubar));
+
+    if env::consts::OS == "darwin" {
+        println!("Copying lib{}.dylib", name);
+        sh.copy_file(format!("target/debug/lib{}.dylib", name), lib_dir)?;
+    } else if env::consts::OS == "linux" {
+        println!("Copying lib{}.so", name);
+        sh.copy_file(format!("target/debug/lib{}.so", name), lib_dir)?;
+    } else if env::consts::OS == "windows" {
+        println!("Copying {}.dll", name);
+        sh.copy_file(format!("target/debug/{}.dll", name), lib_dir)?;
+    }
 
     // Copy dwarf files
     current_dir.push(entry.path());
