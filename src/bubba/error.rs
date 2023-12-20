d8cd7b85f61c54887a641c7e9e6c78ad6efd1916
Keith T. Star <uberfoo@me.com>
2023-12-20 08:13:22 -0700

    Gave VM it's own error type.
    This moved VmPanic out of ChaChaError.
    I also made a local BubbaError for the VM. I'll do the same for the
    compiler.
    
    The end goal is to move the bubba module to a new crate. The rationale
    behind this is so that I can make a VM that runs in WASM. Giving it a
    little thought though, I do not know if this is even doable.
    
    I think I can manage to get the VM to run in WASM, but I do not know if
    I can get the compiler running, since it needs the entire front end.
    The parser is probably not a problem, it's the extruder I'm worried
    about.
    
    And actually that is not the end of the world.

--- src/bubba/error.rs
+++ src/bubba/error.rs
@@ -1,8 +1,17 @@
+use ansi_term::Colour;
 use snafu::{prelude::*, Backtrace, Location};
 
 #[derive(Debug, Snafu)]
 pub struct Error(BubbaError);
-pub(super) type Result<T, E = Error> = std::result::Result<T, E>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+const ERR_CLR: Colour = Colour::Red;
+const OK_CLR: Colour = Colour::Green;
+const POP_CLR: Colour = Colour::Yellow;
+const OTH_CLR: Colour = Colour::Cyan;
 
 #[derive(Debug, Snafu)]
-enum BubbaError {}
+pub(crate) enum BubbaError {
+    #[snafu(display("\n{}: vm panic: {source}", ERR_CLR.bold().paint("error")))]
+    VmPanic { source: Box<dyn std::error::Error> },
+}
