b1d27330b56139ae76b608bc0fa8105aa37609fd
Keith T. Star <uberfoo@me.com>
2024-02-16 11:28:49 -0700

    Resolved merge conflicts by accepting all current changes

--- src/chacha/interpreter/expression/match_expr.rs
+++ src/chacha/interpreter/expression/match_expr.rs
@@ -19,6 +19,8 @@ pub fn eval(
     let match_expr = lu_dog.exhume_x_match(match_expr).unwrap();
     let match_expr = s_read!(match_expr);
 
+    dbg!(&match_expr);
+
     let patterns = match_expr.r87_pattern(&lu_dog);
     let scrutinee = match_expr.r91_expression(&lu_dog)[0].clone();
 
@@ -28,6 +30,7 @@ pub fn eval(
     // 🚧 Darn. Match arms need to be ordered the same as they are written, and
     // they are not ordered in the model.
     for pattern in patterns {
+        dbg!(&pattern);
         let match_expr = s_read!(pattern).r87_expression(&lu_dog)[0].clone();
         let pattern_expr = s_read!(pattern).r92_expression(&lu_dog)[0].clone();
 
