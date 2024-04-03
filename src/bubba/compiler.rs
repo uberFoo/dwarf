bd12b4b7c79b689f7ce9301554e320e45ac9b3c3
Keith T. Star <uberfoo@me.com>
2024-04-03 07:44:08 -0700

    Remember Two Things
    - -c flag to change directory is there, and busted
    - updated README pleading for help

--- src/bubba/compiler.rs
+++ src/bubba/compiler.rs
@@ -963,7 +963,7 @@ mod test {
         .unwrap();
         let program = compile(&ctx).unwrap();
         println!("{program}");
-        assert_eq!(program.get_thonk_card(), 20);
+        assert_eq!(program.get_thonk_card(), 21);
 
         // assert_eq!(program.get_instruction_card(), 393);
         let run = run_vm(&program);
