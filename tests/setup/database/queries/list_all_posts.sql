eccec52260e7f2ded312f66a3c409c31b31c6d3d
Keith T. Star <uberfoo@me.com>
2024-04-25 15:04:16 -0700

    Changed SQL testing
    I changed the setup, and the table. It now has an INT, and I test
    binding to it. Sadly it works.

--- tests/setup/database/queries/list_all_posts.sql
+++
@@ -1,7 +0,0 @@
-SELECT p.id as "post_id",
-       p.title,
-       p.body,
-       u.id as "author_id",
-       u.username as "author_username"
-FROM users u
-         JOIN posts p on u.id = p.user_id;
