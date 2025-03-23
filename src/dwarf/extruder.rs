e9caf909e413d4f1f3cb1a93c484f400b9d90865
Keith T. Star <uberfoo@me.com>
2025-03-23 03:38:57 -0700

    greater than or equal

--- src/dwarf/extruder.rs
+++ src/dwarf/extruder.rs
@@ -1182,58 +1182,14 @@ pub(super) fn inter_expression(
         //
         // GreaterThan: >
         //
-        ParserExpression::GreaterThan(lhs_p, rhs_p) => {
-            gt::inter(lhs_p, rhs_p, span, block, context, import_stack, lu_dog)
+        ParserExpression::GreaterThan(lhs, rhs) => {
+            gt::inter(lhs, rhs, span, block, context, import_stack, lu_dog)
         }
         //
         // GreaterThanOrEqual: >=
         //
-        ParserExpression::GreaterThanOrEqual(ref lhs_p, ref rhs_p) => {
-            let (lhs, lhs_ty) = inter_expression(
-                &new_ref!(ParserExpression, lhs_p.0.to_owned()),
-                &lhs_p.1,
-                block,
-                context,
-                import_stack,
-                lu_dog,
-            )?;
-            let (rhs, rhs_ty) = inter_expression(
-                &new_ref!(ParserExpression, rhs_p.0.to_owned()),
-                &rhs_p.1,
-                block,
-                context,
-                import_stack,
-                lu_dog,
-            )?;
-
-            // 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
-            // 🚧                        THIS IS SUPER IMPORTANT!
-            // 🚧
-            // 🚧 We need to check the types of the LHS and RHS to make sure that they are the same,
-            // 🚧 or at least compatible. Need to look into rust rules.
-            // 🚧 We also need to check that the types implement PartialEq, and whatever else...
-            // 🚧
-            // 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
-
-            typecheck(
-                (&lhs_ty, &lhs_p.1),
-                (&rhs_ty, &rhs_p.1),
-                location!(),
-                context,
-                lu_dog,
-            )?;
-
-            let expr = Comparison::new_greater_than_or_equal(true, lu_dog);
-            let expr = Operator::new_comparison(&lhs.0, Some(&rhs.0), &expr, lu_dog);
-            let expr = Expression::new_operator(true, &expr, lu_dog);
-
-            let ty = Ty::new_boolean(context.sarzak);
-            let ty = ValueType::new_ty(true, &ty, lu_dog);
-
-            let value = XValue::new_expression(block, &ty, &expr, lu_dog);
-            update_span_value(&span, &value, location!());
-
-            Ok(((expr, span), ty))
+        ParserExpression::GreaterThanOrEqual(lhs, rhs) => {
+            gte::inter(lhs, rhs, span, block, context, import_stack, lu_dog)
         }
         //
         // Group
